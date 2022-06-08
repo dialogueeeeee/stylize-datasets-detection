231b466db262cbc63fff98f5621d6650df3376ab
dialogueeeeee <1720817330@qq.com>
2022-06-08 19:48:55 +0800

    增加原始文件。

---
+++ task2-gen.bat
@@ -0,0 +1,11 @@
+python stylize.py --content-dir F:\pyprj_testfile\shape_task2\circle --style-dir F:\graduation_prj\texture-vs-shape_ArticalRec\train --output-dir F:\pyprj_testfile\shape_task2\style\circle-style --num-styles 1 --alpha 0.3 --content-size 0 --style-size 256
+
+python stylize.py --content-dir F:\pyprj_testfile\shape_task2\rect-cir-elli-tri --style-dir F:\graduation_prj\texture-vs-shape_ArticalRec\train --output-dir F:\pyprj_testfile\shape_task2\style\rect-cir-elli-tri-style --num-styles 1 --alpha 0.3 --content-size 0 --style-size 256
+
+python stylize.py --content-dir F:\pyprj_testfile\shape_task2\rect-cir-tri --style-dir F:\graduation_prj\texture-vs-shape_ArticalRec\train --output-dir F:\pyprj_testfile\shape_task2\style\rect-cir-tri-style --num-styles 1 --alpha 0.3 --content-size 0 --style-size 256
+
+python stylize.py --content-dir F:\pyprj_testfile\shape_task2\rect-elli --style-dir F:\graduation_prj\texture-vs-shape_ArticalRec\train --output-dir F:\pyprj_testfile\shape_task2\style\rect-elli-style --num-styles 1 --alpha 0.3 --content-size 0 --style-size 256
+
+python stylize.py --content-dir F:\pyprj_testfile\shape_task2\rect-elli-tri --style-dir F:\graduation_prj\texture-vs-shape_ArticalRec\train --output-dir F:\pyprj_testfile\shape_task2\style\rect-elli-tri-style --num-styles 1 --alpha 0.3 --content-size 0 --style-size 256
+
+python stylize.py --content-dir F:\pyprj_testfile\shape_task2\rect-tri --style-dir F:\graduation_prj\texture-vs-shape_ArticalRec\train --output-dir F:\pyprj_testfile\shape_task2\style\rect-tri-style --num-styles 1 --alpha 0.3 --content-size 0 --style-size 256
